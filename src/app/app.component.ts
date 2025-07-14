b1e7bb39d25780f16eda67c284fecf5076a600ff
fabri <fabri11399ct@gmail.com>
2025-07-14 19:31:45 +0200

    finito ultimo esempio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from '../product';
+import { Signal } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-signals-examples4';
+  
+  allProducts: Product[] = [
+    {id: 1, name: 'milk', price: 1.45},
+    {id: 2, name: 'Bread', price: 3.90},
+    {id: 3, name: 'Tomatoes', price: 2.20}
+  ];
+
+
 }
